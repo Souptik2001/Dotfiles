9461bfe072f94afa9d7dc8c59c4a4d485afde285
Souptik Datta <souptikdatta2001@gmail.com>
2022-03-20 17:09:49 +0530

    chore: Install and configure oh-my-zsh for root user also

--- terminal-setup/install.sh
+++ terminal-setup/install.sh
@@ -1,3 +1,27 @@
 #! /bin/bash
 
+# --------------------------
+# Kitty
+# --------------------------
+
+# Install kitty terminal
+sudo pacman -S kitty
+
+# --------------------------
+# --------------------------
+
+
+# --------------------------
+# OhMyZSH
+# --------------------------
+
+# Install zsh for current user
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install zsh for root user
+sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Next copy the .zshrc file and .oh-my-zsh folder to home folders of both curent user's and root user's home directories.
+
+# --------------------------
+# --------------------------
