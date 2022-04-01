c1f8ad82aa31392bd21a682e57d2b3e52e6d4bb7
Souptik Datta <souptikdatta2001@gmail.com>
2022-04-01 22:47:03 +0530

    feat: Add a script for extracting and converting gpg keys to QR codes

---
+++ password-store-setup/create_gpg_qr.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+## Export the private and public keys
+rm -r gpg_qr_"$1"
+mkdir gpg_qr_"$1"
+gpg --output ./gpg_qr_"$1"/private.key --armor --export-secret-keys $1
+gpg --output ./gpg_qr_"$1"/public.key --armor --export $1
+
+## Split the public and private keys to smaller parts
+rm -r ./gpg_qr_"$1"/private-gpg-splits
+rm -r ./gpg_qr_"$1"/public-gpg-splits
+mkdir ./gpg_qr_"$1"/private-gpg-splits
+mkdir ./gpg_qr_"$1"/public-gpg-splits
+split -C 1000 ./gpg_qr_"$1"/private.key ./gpg_qr_"$1"/private-gpg-splits/privatesplitkey-
+split -C 1000 ./gpg_qr_"$1"/public.key ./gpg_qr_"$1"/public-gpg-splits/publicsplitkey-
+
+## Convert them into QR codes
+rm -r ./gpg_qr_"$1"/private-gpg-splits-qr
+rm -r ./gpg_qr_"$1"/public-gpg-splits-qr
+mkdir ./gpg_qr_"$1"/private-gpg-splits-qr
+mkdir ./gpg_qr_"$1"/public-gpg-splits-qr
+
+count=1
+for file in ./gpg_qr_"$1"/private-gpg-splits/privatesplitkey-??; do
+	<"$file" qrencode -s 3 -d 150 -o ./gpg_qr_"$1"/private-gpg-splits-qr/private-"$count".png
+	((count+=1))
+done
+
+count=1
+for file in ./gpg_qr_"$1"/public-gpg-splits/publicsplitkey-??; do
+	<"$file" qrencode -s 3 -d 150 -o ./gpg_qr_"$1"/public-gpg-splits-qr/public-"$count".png
+	((count+=1))
+done
